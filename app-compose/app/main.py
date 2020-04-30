663e039b46b6b12080346ce07069760319bd2fab
Matheus Abreu <abreumatheus@icloud.com>
2020-04-30 10:41:41 -0300

    Refactored database hostname to match compose file

--- app-compose/app/main.py
+++ app-compose/app/main.py
@@ -4,7 +4,7 @@ from flask_migrate import Migrate
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://admin:123456789@db-server/padawans'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://admin:123456789@database/padawans'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
